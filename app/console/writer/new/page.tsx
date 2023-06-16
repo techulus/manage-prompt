b2a2ea0f6f083bcbfe7397aab38b6afc7dee00ec
Arjun Komath <arjun@hey.com>
2023-06-16 21:31:37 +1000

    remove editor

--- app/console/writer/new/page.tsx
+++ app/console/writer/new/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 
 import { ContentBlock } from "@/components/core/content-block";
-import { EDITOR_JS_TOOLS } from "@/components/editor/tools";
 import PageTitle from "@/components/layout/page-title";
-import { createReactEditorJS } from "react-editor-js";
 
 export default async function Writer() {
-  const ReactEditorJS = createReactEditorJS();
-
   return (
     <>
       <PageTitle title="New Page" backUrl="/console/writer" />
 
-      <ContentBlock>
-        <ReactEditorJS tools={EDITOR_JS_TOOLS} />
-      </ContentBlock>
+      <ContentBlock></ContentBlock>
     </>
   );
 }
