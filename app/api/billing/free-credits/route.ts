decc6f8b9ed7f5e13bc3116cb06b0193e25859d1
Arjun Komath <arjun@hey.com>
2023-11-16 19:02:22 +1100

    Test log

--- app/api/billing/free-credits/route.ts
+++ app/api/billing/free-credits/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function GET(_: Request) {
+export async function GET(req: Request) {
+  console.log("GET /api/billing/free-credits", req.headers);
   return NextResponse.json({
     success: true,
   });
