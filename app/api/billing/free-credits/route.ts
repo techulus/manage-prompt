0cee24d079e4b24e929b4110060723841cb33273
Arjun Komath <arjun@hey.com>
2023-11-16 19:33:22 +1100

    Clean up

--- app/api/billing/free-credits/route.ts
+++ app/api/billing/free-credits/route.ts
@@ -2,12 +2,10 @@ import { prisma } from "@/lib/utils/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  // get the bearer token from the header
   const authToken = (req.headers.get("authorization") || "")
     .split("Bearer ")
     .at(1);
 
-  // if not found OR the bearer token does NOT equal the CRON_SECRET
   if (!authToken || authToken != process.env.CRON_SECRET) {
     return NextResponse.json(
       { error: "Unauthorized" },
