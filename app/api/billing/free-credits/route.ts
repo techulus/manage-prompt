6b9086150bff9a9f7362aa72e552c01c4209cd3f
Arjun Komath <arjun@hey.com>
2023-11-16 19:32:46 +1100

    Update endpoint for free credits

--- app/api/billing/free-credits/route.ts
+++ app/api/billing/free-credits/route.ts
@@ -1,7 +1,30 @@
+import { prisma } from "@/lib/utils/db";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-  console.log("GET /api/billing/free-credits", req.headers);
+  // get the bearer token from the header
+  const authToken = (req.headers.get("authorization") || "")
+    .split("Bearer ")
+    .at(1);
+
+  // if not found OR the bearer token does NOT equal the CRON_SECRET
+  if (!authToken || authToken != process.env.CRON_SECRET) {
+    return NextResponse.json(
+      { error: "Unauthorized" },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  await prisma.organization.updateMany({
+    data: {
+      credits: 100,
+    },
+  });
+
+  console.log("Cron job ran successfully");
+
   return NextResponse.json({
     success: true,
   });
