890b11bc81d651f1f55f2dc1763b004f02843046
Arjun Komath <arjun@hey.com>
2023-11-18 13:52:20 +1100

    Enable writer

--- app/console/writer/page.tsx
+++ app/console/writer/page.tsx
@@ -1,16 +1,29 @@
 import { ContentBlock } from "@/components/core/content-block";
 import PageTitle from "@/components/layout/page-title";
+import { Editor } from "novel";
+
+// TODO: disable image upload
 
 export default async function Writer() {
   return (
     <>
-      <PageTitle
-        title="Writer"
-        actionLabel="New Page"
-        actionLink="/console/writer/new"
-      />
+      <PageTitle title="Writer" />
 
-      <ContentBlock>WIP</ContentBlock>
+      <ContentBlock>
+        <Editor
+          className="max-w-7xl"
+          defaultValue={{
+            type: "doc",
+            content: [
+              {
+                type: "heading",
+                attrs: { level: 2 },
+                content: [{ type: "text", text: "Hello!" }],
+              },
+            ],
+          }}
+        />
+      </ContentBlock>
     </>
   );
 }
