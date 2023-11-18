14e862674d83d59cce20e3562a8f2a094103fe7f
Arjun Komath <arjun@hey.com>
2023-11-18 13:57:34 +1100

    Fix border

--- components/core/content-block.tsx
+++ components/core/content-block.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card } from "../ui/card";
 import { twMerge } from "tailwind-merge";
+import { Card } from "../ui/card";
 
 export const ContentBlock = React.forwardRef<
   HTMLDivElement,
@@ -8,7 +8,7 @@ export const ContentBlock = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <Card
     className={twMerge(
-      "rounded-none border-l-0 border-r-0 xl:border xl:rounded-md xl:mt-4 xl:mx-auto max-w-7xl",
+      "rounded-none border-l-0 border-r-0 xl:border dark:xl:border-gray-800 xl:rounded-md xl:mt-4 xl:mx-auto max-w-7xl",
       className
     )}
     ref={ref}
