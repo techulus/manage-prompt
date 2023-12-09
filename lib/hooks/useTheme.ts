c176e7d8a999b279b93c698cade37c26546ed953
Arjun Komath <arjun@hey.com>
2023-12-10 08:49:35 +1100

    Flex theme change flashing

--- lib/hooks/useTheme.ts
+++ lib/hooks/useTheme.ts
@@ -1,16 +1,22 @@
 "use client";
 
 import { useEffect } from "react";
+import { useCookies } from "react-cookie";
 import usePrefersColorScheme from "./usePrefersColorScheme";
 
 export const useTheme = (): "dark" | "light" => {
+  const [cookies, setCookie] = useCookies(["theme"]);
   const colorScheme = usePrefersColorScheme();
 
   useEffect(() => {
+    if (colorScheme == "no-preference") return;
     const root = window.document.documentElement;
     root.classList.remove("light", "dark");
     root.classList.add(colorScheme);
-  }, [colorScheme]);
+    setCookie("theme", colorScheme, { path: "/" });
+  }, [colorScheme, setCookie]);
 
-  return colorScheme == "no-preference" ? "dark" : colorScheme;
+  return colorScheme == "no-preference"
+    ? cookies.theme ?? "light"
+    : colorScheme;
 };
