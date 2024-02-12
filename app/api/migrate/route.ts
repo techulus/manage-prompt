9f85cd1038acdd0514b3f5a0de72b4488173451c
Arjun Komath <arjun@hey.com>
2024-02-12 22:33:03 +1100

    Clean migration

--- app/api/migrate/route.ts
+++ app/api/migrate/route.ts
@@ -1,21 +1,6 @@
-import { prisma } from "@/lib/utils/db";
 import { NextResponse } from "next/server";
-import { randomBytes } from "node:crypto";
 
 export async function GET(_: Request) {
-  const workflows = await prisma.workflow.findMany();
-
-  for (const workflow of workflows) {
-    await prisma.workflow.update({
-      where: {
-        id: workflow.id,
-      },
-      data: {
-        shortId: `wf_${randomBytes(16).toString("hex")}`,
-      },
-    });
-  }
-
   return NextResponse.json({
     message: "ok",
   });
