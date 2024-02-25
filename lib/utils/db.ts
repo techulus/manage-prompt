a54a7982abfd0a8a8c44f8d2b7dd13b7a6bc7b18
Arjun Komath <arjun@hey.com>
2024-02-25 14:46:53 +1100

    Try Prisma accelerate

--- lib/utils/db.ts
+++ lib/utils/db.ts
@@ -1,24 +1,16 @@
-import { neonConfig, Pool, PoolConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
-import ws from "ws";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 // types
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: any;
 }
-
-// setup
-neonConfig.webSocketConstructor = ws;
-const poolConfig: PoolConfig = { connectionString: process.env.DATABASE_URL };
-
-// instantiate
-const pool = new Pool(poolConfig); // log:console.log?
-const adapter = new PrismaNeon(pool);
 
 export const prisma =
   global.prisma ||
-  new PrismaClient({ adapter, log: ["info", "warn", "error"] });
+  new PrismaClient({ log: ["info", "warn", "error"] }).$extends(
+    withAccelerate()
+  );
 
 // cache on global for HOT RELOAD in dev
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
