c6325cf575062b86aa1855f27b6c45773947946b
Arjun Komath <arjun@hey.com>
2024-02-25 15:27:10 +1100

    ignore info

--- lib/utils/db.ts
+++ lib/utils/db.ts
@@ -8,9 +8,7 @@ declare global {
 
 export const prisma =
   global.prisma ||
-  new PrismaClient({ log: ["info", "warn", "error"] }).$extends(
-    withAccelerate()
-  );
+  new PrismaClient({ log: ["warn", "error"] }).$extends(withAccelerate());
 
 // cache on global for HOT RELOAD in dev
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
