8a4d27b259cffdbb466dbb4ca51fde400b4660ce
Arjun Komath <arjun@hey.com>
2024-07-19 23:00:12 +1000

    Cache workflow query in stream endpoint

--- app/api/v1/run/[workflowId]/stream/route.ts
+++ app/api/v1/run/[workflowId]/stream/route.ts
@@ -97,6 +97,9 @@ export async function POST(
       where: {
         shortId: params.workflowId,
       },
+      cacheStrategy: {
+        ttl: 60,
+      },
     });
     if (!workflow || !workflow?.published) {
       return ErrorResponse("Workflow not found", 404);
