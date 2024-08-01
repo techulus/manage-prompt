2f24109e9b78e70ad7e0414ca29f3275aa6be4ca
Arjun Komath <arjun@hey.com>
2024-08-01 21:49:15 +1000

    Add caching

--- app/api/v1/run/[workflowId]/stream/route.ts
+++ app/api/v1/run/[workflowId]/stream/route.ts
@@ -89,6 +89,9 @@ export async function POST(
       where: {
         shortId: params.workflowId,
       },
+      cacheStrategy: {
+        ttl: 60,
+      },
     });
     if (!workflow || !workflow?.published) {
       return ErrorResponse("Workflow not found", 404);
