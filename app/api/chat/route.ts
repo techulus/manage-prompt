d974ab3d167211be246d435bbf1390b9a3891356
Arjun Komath <arjun@hey.com>
2024-08-10 16:14:54 +1000

    feat: Add RAGChat component and API route for chat functionality

---
+++ app/api/chat/route.ts
@@ -0,0 +1,25 @@
+import { owner } from "@/lib/hooks/useOwner";
+import { ragChat } from "@/lib/utils/rag-chat";
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { ownerId } = await owner();
+
+  const { messages } = await req.json();
+  const lastMessage = messages[messages.length - 1].content;
+
+  const response = await ragChat.chat(lastMessage, {
+    namespace: ownerId,
+    streaming: true,
+    sessionId: ownerId,
+    ratelimit: new Ratelimit({
+      redis: Redis.fromEnv(),
+      limiter: Ratelimit.slidingWindow(10, "10 s"),
+    }),
+  });
+
+  return aiUseChatAdapter(response);
+}
