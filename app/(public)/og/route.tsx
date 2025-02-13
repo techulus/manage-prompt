5a150283c4fd1a24c37136102b93460d1239cd86
Arjun Komath <arjun@hey.com>
2025-02-13 20:09:51 +1100

    Clean up

--- app/(public)/og/route.tsx
+++ app/(public)/og/route.tsx
@@ -1,8 +1,4 @@
 import { ImageResponse } from "next/og";
-// App router includes @vercel/og.
-// No need to install it.
-
-export const runtime = "edge";
 
 export async function GET(request: Request) {
   try {
--- lib/utils/db.ts
+++ lib/utils/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 export const prisma = new PrismaClient().$extends(withAccelerate());
