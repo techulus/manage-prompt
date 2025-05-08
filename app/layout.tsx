765e123a4f15cae1acab2e0ba482116aab75afce
Arjun Komath <arjun@hey.com>
2025-05-08 18:30:36 +1000

    Add openpanel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/core/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { SITE_METADATA } from "@/data/marketing";
+import { OpenPanelComponent } from "@openpanel/nextjs";
 import classNames from "classnames";
 import { IBM_Plex_Sans } from "next/font/google";
 import Script from "next/script";
@@ -99,6 +100,14 @@ export default async function RootLayout({
       </head>
 
       <body className="flex-1 min-h-full min-w-full tracking-tight">
+        {process.env.NEXT_PUBLIC_OPEN_PANEL_CLIENT_ID ? (
+          <OpenPanelComponent
+            clientId={process.env.NEXT_PUBLIC_OPEN_PANEL_CLIENT_ID}
+            trackScreenViews
+            trackAttributes
+            trackOutgoingLinks
+          />
+        ) : null}
         <ThemeProvider>{children}</ThemeProvider>
         <Script
           strategy="afterInteractive"
